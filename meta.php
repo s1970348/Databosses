7a250d3074976854360fef698187d5670e46481c
s1970348 <j.j.veentjer@student.rug.nl>
2012-12-13 14:12:46 +0100

    Update meta.php

--- meta.php
+++ meta.php
@@ -1,7 +1,7 @@
 <meta name="Databosses,online library,books,e-books,book storage,book logs,book worm,book storage,databosses,data boss,databoss,datab" content="HTML,PHP, CSS, XHTML, JavaScript" /> 
 <meta name="author" content="Jesse Veentjer,Anneke Hogenbirk,Peter Zwama, Patrick Derichs" />
 <meta name="description" content="Website for the Databosses library" />
-<meta name="revised" content="Jesse Veentjer, 30/07/2012" />
+<meta name="revised" content="Jesse Veentjer, 13/12/2012" />
 <meta http-equiv="refresh" content="5" />
 
 <?php
