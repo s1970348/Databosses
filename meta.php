8e7724c7b73bfcbe9d344f9fe165862d9e8e4acd
s1970348 <j.j.veentjer@student.rug.nl>
2012-12-13 16:50:06 +0100

    Update meta.php

--- meta.php
+++ meta.php
@@ -3,4 +3,7 @@
 <meta name="description" content="Website for the Databosses library" />
 <meta name="revised" content="Jesse Veentjer, 13/12/2012" />
 <meta http-equiv="refresh" content="5" />
+<?
+date_default_timezone_set('GMT');
+?>
 
